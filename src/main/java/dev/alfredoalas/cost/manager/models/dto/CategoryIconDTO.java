56a7df0e65d4645e29410152649e19adfb8f3661
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-02 15:52:18 -0600

    Adds CategoryIconDTO.java

---
+++ src/main/java/dev/alfredoalas/cost/manager/models/dto/CategoryIconDTO.java
@@ -0,0 +1,18 @@
+package dev.alfredoalas.cost.manager.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryIconDTO {
+
+    private int idCategoryIcon;
+
+    private String categoryIcon;
+
+}

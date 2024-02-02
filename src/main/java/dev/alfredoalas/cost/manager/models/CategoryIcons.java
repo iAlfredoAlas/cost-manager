1f69ced2b32db51c4fa3c200dd6588b8a8b53e28
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-02 14:28:08 -0600

    Adds CategoryIcons.java

---
+++ src/main/java/dev/alfredoalas/cost/manager/models/CategoryIcons.java
@@ -0,0 +1,29 @@
+package dev.alfredoalas.cost.manager.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "category_icons")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryIcons implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_category_icon")
+    private int idAccount;
+
+    @Basic(optional = false)
+    @Column(name = "category_icon")
+    private String categoryIcon;
+
+}

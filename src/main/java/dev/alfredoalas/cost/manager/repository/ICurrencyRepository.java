f75c351898ac84a2a169ef4be5e4631718d127c1
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-08 14:39:26 -0600

    Adds ICurrencyRepository.java

---
+++ src/main/java/dev/alfredoalas/cost/manager/repository/ICurrencyRepository.java
@@ -0,0 +1,6 @@
+package dev.alfredoalas.cost.manager.repository;
+
+import dev.alfredoalas.cost.manager.models.Currency;
+
+public interface ICurrencyRepository extends IGenericRepository<Currency, Integer>{
+}

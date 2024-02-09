03d598d363cb2d8097c8eaaf773a0ed7de93429d
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-09 15:21:24 -0600

    feat: Adds IAccountService.java

---
+++ src/main/java/dev/alfredoalas/cost/manager/services/IAccountService.java
@@ -0,0 +1,6 @@
+package dev.alfredoalas.cost.manager.services;
+
+import dev.alfredoalas.cost.manager.models.Account;
+
+public interface IAccountService extends ICrudGenericService<Account, Integer>{
+}

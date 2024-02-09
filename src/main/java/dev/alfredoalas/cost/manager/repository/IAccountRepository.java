e023f921d55b545aa69e407c4cee104c36263f81
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-09 14:41:17 -0600

    Refactor: Changes type of method

--- src/main/java/dev/alfredoalas/cost/manager/repository/IAccountRepository.java
+++ src/main/java/dev/alfredoalas/cost/manager/repository/IAccountRepository.java
@@ -1,9 +1,9 @@
 package dev.alfredoalas.cost.manager.repository;
 
 import dev.alfredoalas.cost.manager.models.Account;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IAccountRepository extends IGenericRepository<Account, Integer>{
-    Page<Account> findAccountByIsBalance (Pageable pageable, Boolean isBalance);
+    List<Account> findAccountByIsBalance (Boolean isBalance);
 }

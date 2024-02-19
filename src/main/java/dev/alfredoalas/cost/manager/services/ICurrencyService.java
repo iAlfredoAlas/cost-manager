d41e35e9cb72807546cae17d8807be1b0c046aec
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-19 14:53:10 -0600

    feat: Adds ICurrencyService.java

---
+++ src/main/java/dev/alfredoalas/cost/manager/services/ICurrencyService.java
@@ -0,0 +1,6 @@
+package dev.alfredoalas.cost.manager.services;
+
+import dev.alfredoalas.cost.manager.models.Currency;
+
+public interface ICurrencyService extends ICrudGenericService<Currency, Integer>{
+}

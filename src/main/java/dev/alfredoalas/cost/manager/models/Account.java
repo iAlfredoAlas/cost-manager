e9fd342d98786b5171bb77c94c9bf593cf457aab
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-19 16:02:45 -0600

    docs: Adds documentation to Account.java

--- src/main/java/dev/alfredoalas/cost/manager/models/Account.java
+++ src/main/java/dev/alfredoalas/cost/manager/models/Account.java
@@ -1,4 +1,8 @@
 package dev.alfredoalas.cost.manager.models;
+/**
+ * Represents a financial account used for tracking income and expenses.
+ * @autor Alfredo Alas
+ */
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -18,28 +22,47 @@ import java.time.LocalDate;
 @Setter
 public class Account implements Serializable {
 
+    /**
+     * The unique identifier of the account.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id_account")
     private int idAccount;
 
+    /**
+     * The descriptive name of the account.
+     */
     @Basic(optional = false)
     @Column(name = "name_account")
     private String nameAccount;
 
+    /**
+     * The initial balance of the account.
+     */
     @Basic(optional = false)
     @Column(name = "initial_account_balance")
     private BigDecimal initialAccountBalance;
 
+    /**
+     * The date the initial balance was established.
+     */
     @Basic(optional = false)
     @Column(name = "initial_balance_date")
     private LocalDate intialAccountDate;
 
+    /**
+     *Indicates whether the account should be included in the balance sheet (true) or
+     * excluded from the balance sheet (false).
+     */
     @Basic(optional = false)
     @Column(name = "balance")
     private Boolean isBalance;
 
+    /**
+     * The currency associated with the account.
+     */
     @JoinColumn(name = "id_currency", referencedColumnName = "id_currency", foreignKey = @ForeignKey(name = "FK_accounts_currencies"))
     @ManyToOne(optional = false, targetEntity = Currency.class)
     private Currency idCurrency;

b919d7ecff80e726e970c23b9d5bcd9f5351205f
Alfredo Alas <alfredo.alas14@gmail.com>
2024-02-19 16:04:38 -0600

    docs: Adds documentation to Category.java

--- src/main/java/dev/alfredoalas/cost/manager/models/Category.java
+++ src/main/java/dev/alfredoalas/cost/manager/models/Category.java
@@ -1,4 +1,8 @@
 package dev.alfredoalas.cost.manager.models;
+/**
+ * Represents a category for classifying financial transactions.
+ * @author Alfredo Alas
+ */
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -16,16 +20,25 @@ import java.io.Serializable;
 @Setter
 public class Category implements Serializable {
 
+    /**
+     * The unique identifier of the category.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id_category")
     private int idCategory;
 
+    /**
+     * The descriptive name of the category (e.g., "Utilities", "Groceries").
+     */
     @Basic(optional = false)
     @Column(name = "name_category")
     private String nameCategory;
 
+    /**
+     * The icon associated with the category.
+     */
     @JoinColumn(name = "id_category_icon", referencedColumnName = "id_category_icon", foreignKey = @ForeignKey(name = "FK_categories_category_icons"))
     @ManyToOne(optional = false, targetEntity = CategoryIcon.class)
     private CategoryIcon idCategoryIcon;
